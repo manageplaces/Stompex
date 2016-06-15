1ebba559a0ececd6af70b7673605361b2a56edd5
Adrian Hooper <adrian@euphoricpanda.co.uk>
2016-06-15 15:22:54 +0100

    Initial

---
+++ lib/stompex.ex
@@ -0,0 +1,16 @@
+defmodule Stompex do
+  @doc """
+  """
+
+  @doc """
+  """
+  def connect(host, login, passcode) do
+  end
+
+  def connect(host, port, login, passcode) do
+  end
+
+  def connect(host, port, login, passcode, headers) do
+  end
+
+end
---
+++ test/stompex_test.exs
@@ -0,0 +1,8 @@
+defmodule StompexTest do
+  use ExUnit.Case
+  doctest Stompex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
