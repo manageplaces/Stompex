175b2917faffdb7b528781ccf9d5bdbcbf02db10
Adrian Hooper <adrian@euphoricpanda.co.uk>
2017-06-07 19:15:07 +0100

    Added function for sending pre-built frames

--- lib/stompex.ex
+++ lib/stompex.ex
@@ -147,6 +147,19 @@ defmodule Stompex do
     { :disconnect, { :close, from }, state }
   end
 
+
+  @doc false
+  def handle_call({ :send, destination, %Stompex.Frame{} = frame }, _, %{ sock: sock } = state) do
+    frame =
+      frame
+      |> put_header("destination", destination)
+      |> put_header("content-length", byte_size(frame.body))
+      |> finish_frame()
+
+    response = :gen_tcp.send(sock, frame)
+    { :reply, response, state }
+  end
+
   @doc false
   def handle_call({ :send, destination, message }, _, %{ sock: sock } = state) do
     frame =
